1a122112f98c8276fa168dcc92448630ce8a1869
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2020-09-01 17:36:16 +0300

    task2-4 start

--- js/task2-4.js
+++ js/task2-4.js
@@ -1,18 +1,48 @@
-function formatString(string, maxLength = 40) {
-  // Write code under this line
+/**
+ * Напиши функцию formatString(string) которая принимает строку и форматирует ее если необходимо.
+
+Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
+Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
+ */
+const formatString = function (string) {
+  // твой код
+  const maxLength = 40;
   const stringLength = string.split("");
   if (stringLength.length > maxLength) {
-    stringLength.length = maxLength;
-    return stringLength.join("") + "...";
-  } else if ((stringLength.length = maxLength || stringLength.length < maxLength)) {
-    return stringLength.join("");
   }
-}
+};
 
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
 console.log(formatString("Curabitur ligula sapien, tincidunt non."));
-// 'Curabitur ligula sapien, tincidunt non.'
+// вернется оригинальная строка
 
 console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
-// 'Vestibulum facilisis, purus nec pulvinar...'
+// вернется форматированная строка
 
-console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30));
+console.log(formatString("Curabitur ligula sapien."));
+// вернется оригинальная строка
+
+console.log(formatString("Nunc sed turpis. Curabitur a felis in nunc fringilla tristique."));
+// вернется форматированная строка
+
+// - решение в боте
+// function formatString(string, maxLength = 40) {
+//   // Write code under this line
+//   const stringLength = string.split("");
+//   if (stringLength.length > maxLength) {
+//     stringLength.length = maxLength;
+//     return stringLength.join("") + "...";
+//   } else if ((stringLength.length = maxLength || stringLength.length < maxLength)) {
+//     return stringLength.join("");
+//   }
+// }
+
+// console.log(formatString("Curabitur ligula sapien, tincidunt non."));
+// // 'Curabitur ligula sapien, tincidunt non.'
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis."));
+// // 'Vestibulum facilisis, purus nec pulvinar...'
+
+// console.log(formatString("Vestibulum facilisis, purus nec pulvinar iaculis.", 30));
